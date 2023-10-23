79098f9d6bd1226104132ec68b3e1f6547f8fc57
obpalomo <obpalomo@gmail.com>
2023-10-23 19:37:32 +0200

    update fibo

--- ej_opcionales.js
+++ ej_opcionales.js
@@ -1,4 +1,4 @@
-/* 
+/*
 EJERCICIO 7: (OPCIONALES)
 Completa los datos de la tabla en index.html utilizando los valores del siguiente mapa.
 */
@@ -6,18 +6,18 @@ let toysMap = new Map();
 let newToysArray = new Array();
 let toy1 = 	{id: 23, name: 'Barbie Man'}; // 15
 let toy2 = {id: 40, name: 'El gato con Guantes'}; // 8
-let toy3 = {id: 40, name: 'El gato felix'}; // 35 
+let toy3 = {id: 40, name: 'El gato felix'}; // 35
 toysMap.set(toy1, 15);
 toysMap.set(toy2, 8);
 toysMap.set(toy3, 35);
-let rowAcolA = 'algo'; // extraer de mi mapa el parametro A del juguete uno. 
-let rowAcolB = 'algo'; 
-let rowAcolC = 'algo'; 
-let rowBcolA = 'algo'; // extraer de mi mapa el parametro A del juguete uno. 
-let rowBcolB = 'algo'; 
+let rowAcolA = 'algo'; // extraer de mi mapa el parametro A del juguete uno.
+let rowAcolB = 'algo';
+let rowAcolC = 'algo';
+let rowBcolA = 'algo'; // extraer de mi mapa el parametro A del juguete uno.
+let rowBcolB = 'algo';
 let rowBcolC = 'algo';
-let rowCcolA = 'algo'; // extraer de mi mapa el parametro A del juguete uno. 
-let rowCcolB = 'algo'; 
+let rowCcolA = 'algo'; // extraer de mi mapa el parametro A del juguete uno.
+let rowCcolB = 'algo';
 let rowCcolC = 'algo';
 
 /*
@@ -30,6 +30,11 @@ Se pide:
 
 - Crear un codigo que almacene en un array los n primeros números de la sucesion (n se definirá como let n = algo).
 - Definir con palabras (o pseudocodigo) como vamos a afrontar la solución del problema (que requsitos tiene nuestro programa y como queremos resolverlos).
+        /* -Declarar las posiciones 1 y 2 del array
+        -Recorres un array hasta 15 posiciones
+        -Crear una lógica de la suma fibonacci
+        -Impular numeros al array */
+
 - Implementar esa estrategia utilizando código.
 - testear usando los valores para n: 8, 15, 16. (imprimir el resultado con console.log);
 
